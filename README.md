b8b83e3efbb8245ba96d182769a4ccaea16d1faf
Samir <118690585+sr5434@users.noreply.github.com>
2023-06-09 15:33:52 +0000

    Add source code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # timezone-detect
-Detect timezones with express
+Detect timezones with express.js
+## Get started
+Run `node .` and then `sh test.sh`
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { find } = require('geo-tz')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/coords_to_tz', (req, res) => {
+    //tz = find(req.location, req.long)
+    let lat = req.body.lat;
+    let long = req.body.long;
+    let tz = find(lat, long)
+    res.send({"timezone": tz});
+})
+
+app.listen(8080, () => {
+    console.log(`Example app listening on port 8080`)
+})
