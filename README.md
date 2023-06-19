ec03367aaede75be1d476cd1457029ea203225af
Samir <118690585+sr5434@users.noreply.github.com>
2023-06-19 18:07:08 +0000

    Add unit tests

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # timezone-detect
-Detect timezones with express.js
+Detect timezones with a RESTful API.
 ## Get started
-Run `node .` and then `sh test.sh`
+Run `node .` and then run `sh test.sh` in another window to try out the API.
+## Unit Tests
+To run the unit tests, run `node .` and then `npx jest` in another window.
---
+++ test/index.test.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const baseURL = "http://localhost:8080";
+
+describe("POST /coords_to_tz", () => {
+    it("should return correct timezone, test 1", async () => {
+      const coords = {"lat": 44.140624, "long": -73.958437};
+      const response = await request(baseURL).post("/coords_to_tz").send(coords);
+      expect(response.body["timezone"][0]).toBe("America/New_York");
+    });
+    it("should return correct timezone, test 2", async () => {
+      const coords2 = {"lat": 40.372834, "long": -121.757330};
+      const response2 = await request(baseURL).post("/coords_to_tz").send(coords2);
+      expect(response2.body["timezone"][0]).toBe("America/Los_Angeles");
+    });
+    it("should return correct timezone, test 3", async () => {
+      const coords3 = {"lat": 45.124646, "long": -79.677548};
+      const response3 = await request(baseURL).post("/coords_to_tz").send(coords3);
+      expect(response3.body["timezone"][0]).toBe("America/Toronto");
+    });
+});
