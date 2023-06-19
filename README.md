45c14ecc25a980c9512ad97c0692d0c2515191e1
Samir <118690585+sr5434@users.noreply.github.com>
2023-06-19 18:27:43 +0000

    Run prettier on the code

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # timezone-detect
+
 Detect timezones with a RESTful API.
+
 ## Get started
+
 Run `node .` and then run `sh test.sh` in another window to try out the API.
+
 ## Unit Tests
+
 To run the unit tests, run `node .` and then `npm test` in another window.
--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express = require('express')
-const { find } = require('geo-tz')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require("express");
+const { find } = require("geo-tz");
+const bodyParser = require("body-parser");
+const app = express();
 
-function timeInTZ(tz){
-    let options = {
-        timeZone: tz,
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-      },
-      formatter = new Intl.DateTimeFormat([], options);
-    
-    return formatter.format(new Date());
+function timeInTZ(tz) {
+  let options = {
+      timeZone: tz,
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    },
+    formatter = new Intl.DateTimeFormat([], options);
+
+  return formatter.format(new Date());
 }
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/coords_to_tz', (req, res) => {
-    //tz = find(req.location, req.long)
-    let lat = req.body.lat;
-    let long = req.body.long;
-    let tz = find(lat, long);
-    let times = [];
-    for (let i = 0; i < tz.length; i++){
-        times.push(timeInTZ(tz[i]))
-    }
-    res.send({"timezone": tz, "time": times});
-})
+app.post("/coords_to_tz", (req, res) => {
+  //tz = find(req.location, req.long)
+  let lat = req.body.lat;
+  let long = req.body.long;
+  let tz = find(lat, long);
+  let times = [];
+  for (let i = 0; i < tz.length; i++) {
+    times.push(timeInTZ(tz[i]));
+  }
+  res.send({ timezone: tz, time: times });
+});
 
 app.listen(8080, () => {
-    console.log(`Example app listening on port 8080`)
-})
+  console.log(`Example app listening on port 8080`);
+});
 module.exports = app;
