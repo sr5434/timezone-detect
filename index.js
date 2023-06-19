87118256484ca5c51aaf7768bada7edf9de7e17f
Samir <118690585+sr5434@users.noreply.github.com>
2023-06-19 17:42:09 +0000

    return time along with timezone

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const { find } = require('geo-tz')
 const bodyParser = require('body-parser')
 const app = express()
 
+function timeInTZ(tz){
+    let options = {
+        timeZone: tz,
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      },
+      formatter = new Intl.DateTimeFormat([], options);
+    
+    return formatter.format(new Date());
+}
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -10,8 +25,12 @@ app.post('/coords_to_tz', (req, res) => {
     //tz = find(req.location, req.long)
     let lat = req.body.lat;
     let long = req.body.long;
-    let tz = find(lat, long)
-    res.send({"timezone": tz});
+    let tz = find(lat, long);
+    let times = [];
+    for (let i = 0; i < tz.length; i++){
+        times.push(timeInTZ(tz[i]))
+    }
+    res.send({"timezone": tz, "time": times});
 })
 
 app.listen(8080, () => {
